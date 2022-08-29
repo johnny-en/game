dd6342c1dca80e8983629e1d2e8174406f08221d
johnny_en <johnny_2008@aliyun.com>
2022-08-29 23:09:56 +0800

    style: 全局样式，暴力关闭警告

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "./runtime-canvas";
 import { getRootContainer } from "./game";
 import App from "./App.vue";
+import "@/styles/index.scss";
 
 // vue template -> render -> vnode -> mountElement -> appendRootContainer
 
 // pixijs
 
 // canvas renderer
-
+console.warn = () => {};
 createApp(App).mount(getRootContainer());

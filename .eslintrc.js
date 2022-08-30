9c299060ec07ddb0c11456cdae06185f43d6d0d1
johnny_en <johnny_2008@aliyun.com>
2022-08-30 22:36:33 +0800

    refactor: 调整 eslint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,14 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "vue/setup-compiler-macros": true,
   },
   parser: "vue-eslint-parser",
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
-  rules: {},
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
 };
